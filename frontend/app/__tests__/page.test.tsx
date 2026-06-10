fdbe770468e064bb3c0c5274e11549ca47b34b60
Jishnu Duhan <duhanjishnu@gmail.com>
2026-06-10 11:29:35 +0530

    fix formatting

--- frontend/app/__tests__/page.test.tsx
+++ frontend/app/__tests__/page.test.tsx
@@ -4,7 +4,20 @@ import Home from '../page';
 
 // Mock next/image since it is optimized for server-side environments
 vi.mock('next/image', () => ({
-  default: ({ src, alt, width, height, priority, ...props }: { src: string; alt: string; width?: number; height?: number; priority?: boolean; [key: string]: unknown }) => {
+  default: ({
+    src,
+    alt,
+    width,
+    height,
+    ...props
+  }: {
+    src: string;
+    alt: string;
+    width?: number;
+    height?: number;
+    priority?: boolean;
+    [key: string]: unknown;
+  }) => {
     // eslint-disable-next-line @next/next/no-img-element
     return <img src={src} alt={alt} width={width} height={height} {...props} />;
   },
